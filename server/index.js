6e344217a1c0ca7bcce579d9aca37324982323a1
Jay <jaydcleverly@gmail.com>
2023-08-26 13:21:42 +0100

    Added basic database functionality for user manipulation

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const mongoose = require("mongoose");
 
 const app = express();
+app.use(express.json());
+
 mongoose.connect(
     "mongodb+srv://jaycleverly:LegoMarvel10!@cluster0.4jcac9j.mongodb.net/reviewsite?retryWrites=true&w=majority"
     );
@@ -9,3 +11,5 @@ mongoose.connect(
 app.listen(3001, () => {
     console.log("SERVER RUNNING!")
 });
+
+require("./routes")(app);
---
+++ server/models/Users.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+})
+
+const UserModel = mongoose.model("users", UserSchema);
+module.exports = UserModel;
